ffe0fe7b3c7052cff6524be8609ba7e5a569107e
OskarToborowicz <otoborowicz@gmail.com>
2026-07-06 17:26:08 +0200

    fix: zmień ikonę barbara na miecz

--- src/components/ClassIcon.tsx
+++ src/components/ClassIcon.tsx
@@ -12,7 +12,7 @@ const ClassIcon: React.FC<ClassIconProps> = ({ className }) => {
       viewBox="0 0 24 24"
       fill="currentColor"
     >
-      <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+      <path d="M12 2L15 5L12 8L9 5L12 2ZM12 10L15 13L12 16L9 13L12 10ZM12 18L15 21L12 24L9 21L12 18Z" />
     </svg>
   );
 };
