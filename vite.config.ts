45726ac9c3b0726632c7f62ca74afb2214e1e334
OskarToborowicz <otoborowicz@gmail.com>
2026-07-22 21:37:25 +0200

    github fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  plugins: [react(), svgr()],
-  base: "/dungeoneers/",
-  resolve: {
-    dedupe: ["react", "react-dom"],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), svgr()],
+    base: env.VITE_BASE_PATH || "/",
+    resolve: {
+      dedupe: ["react", "react-dom"],
+    },
+  };
 });
